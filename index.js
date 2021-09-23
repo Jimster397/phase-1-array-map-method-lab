cff9b7106327a2212cd4b7d680e7803677688f2a
James <jamesschmidt124@gmail.com>
2021-09-22 20:05:52 -0700

    commit tree 784fa7592aff38c9ac5725f90c2d2a436c53f0ce

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = () => {
+//   tutorials.map(function(cb, context){
+//     return titleCased.charAt(0).toUpperCase() + titleCased.slice(1)
+//   }) 
+  
+  
+//   //return tutorials
+// }
+// const caps = (cb, context).split(' ').map(capitalize).join(" ")
+// caps;
+// function titleCased () {
+//     let newArray = tutorials.map((array) => {
+//     return array.split(" ") + array.charAt(0).toUpperCase() + array.join(' ') //+ array.substr(1) 
+//   }); 
+//   return newArray;
+//   }
+  
+  //tutorials.map(tutorial => 
+  // tutorial.charAt(0).toUpperCase()+ tutorial.substr(1));
+  //console.log(tutorials)
+  const titleCased = () => {
+    let newOne = tutorials.map(function whatever(element){
+      let arrayOfWords = element.split(" ");
+      let innerArray = arrayOfWords.map(function(elements){
+        let capitals = elements[0].toUpperCase() + elements.slice(1)
+        return capitals
+      })
+      let final =innerArray.join(' ')
+      console.log(final)
+      return final;
+    })
+    return newOne
+  }
+  console.log(titleCased())
